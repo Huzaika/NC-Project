f2e9cc9e840b3a2a6174bbac0b81818a75517599
Huzaika <38979454+Huzaika@users.noreply.github.com>
2021-07-15 16:17:53 +0500

    Add files via upload

---
+++ REgula falsi project.m
@@ -0,0 +1,31 @@
+clc
+
+syms x;
+
+y = input('Enter non-linear equations: ');
+a = input('Enter first guess: ');
+b = input('Enter second guess: ');
+e = input('Tolerable error: ');
+
+fa = eval(subs(y,x,a));
+fb = eval(subs(y,x,b));
+
+
+if fa*fb > 0 
+    disp('Given initial values do not bracket the root.');
+else
+    c = a - (a-b) * fa/(fa-fb);
+    fc = eval(subs(y,x,c));
+    fprintf('\n\na\t\t\tb\t\t\tc\t\t\tf(c)\n');
+    while abs(fc)>e
+        fprintf('%f\t%f\t%f\t%f\n',a,b,c,fc);
+        if fa*fc< 0
+            b =c;
+            fb = eval(subs(y,x,b));
+        else
+            a =c;
+            fa = eval(subs(y,x,a));
+        end
+        c = a - (a-b) * fa/(fa-fb);
+        fc = eval(subs(y,x,c));
+    end
